13462cad0c7eecf5519796470155d9799ef00c95
Axel Resnik <axelresnik@gmail.com>
2019-06-18 13:53:02 +0300

    views done, logic correct

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ApplicationRecord
+  RAING = [0, 1, 2, 3, 4, 5]
   validates :content, presence: true
-  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  validates :rating, presence: true, inclusion: { in: RAING }
   validates :rating, numericality: { only_integer: true }
   belongs_to :restaurant
 end
